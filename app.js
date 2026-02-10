9c45cf5eb30d0de8269a0766ef20176c260900ac
Rishank Kumar <rishankdhiman072006@gmail.com>
2026-02-10 22:05:09 +0530

    add conditionals to handle addexpense and getexpense

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ import initDB from './db/initDB.js';
 import getExpense from './controllers/getExpense.js';
 import database from './db/database.js';
 import 'dotenv/config';
+import url from 'node:url';
+import createUser from './controllers/createUser.js';
+import { create } from 'node:domain';
+import addExpense from './controllers/addExpense.js';
 
-const server = http.createServer(function(req , res){
+const server = http.createServer(async function(req , res){
     res.setHeader('Access-Control-Allow-Origin' , 'http://localhost:3000');
+    const reqUrl = new URL(req.url , `http://${req.headers.host}`);
 
     try{
     if(req.method === 'OPTIONS'){
@@ -17,27 +22,66 @@ const server = http.createServer(function(req , res){
         res.end('This data is not for user')
         return;
     }
-    else if(req.method === 'POST'){
+
+    else if(req.method === 'POST' && reqUrl.pathname === '/signUp'){
         res.statusCode = 201;
-        res.setHeader('Content-Type' , 'text')
-        res.end('New user is created');
+        try{
+            await createUser(reqUrl.searchParams.get('username') , reqUrl.searchParams.get('email') , reqUrl.searchParams.get('password'))
+            res.end('User Created');
+            return;
+        } catch(error){
+            res.end('Cannot able to create user');
+            return;
+        }
+    }
+
+    else if(req.method === 'GET' && reqUrl.pathname === '/getExpense'){
+        try{
+            const expenceData = await getExpense(reqUrl.searchParams.get('email') , reqUrl.searchParams.get('password'));
+            if(expenceData.length === 0){
+                res.statusCode = 404;
+                res.end();
+                return;
+            }
+            else{
+                res.end(JSON.stringify(expenceData));
+                return;
+            }
+        } catch (error){
+            res.end(`Cannot get expense ${error}`);
+            return;
+        }
+    }
+
+    else if(req.method === 'POST' && reqUrl.pathname === '/addExpense'){
+        const result = await addExpense(reqUrl.searchParams.get('userId') , reqUrl.searchParams.get('amnt') , reqUrl.searchParams.get('dscr'));
+        if(result === 0){
+            res.end('Added expense');
+            return;
+        }
+        else{
+            res.end('Failed to add expense');
+            return;
+        }
+    }
+    
+    else if(req.method === 'GET' && reqUrl.pathname === '/getExpense'){
+        const result = await getExpense(reqUrl.searchParams.get('email') , reqUrl.searchParams.get('password'));
+        if(result === 1){
+            res.end('cannot fetch data');
+            return;
+        }
+        else{
+            res.end(result);
+            return;
+        }
+    }
+
+    else{
+        res.end('invalid request');
         return;
     }
-        else if(req.method === 'GET'){
-        res.end();
-        return;
-    }
-    else if(req.method === 'DELETE'){
-        res.end();
-        return;
-    }
-    else if(req.method === 'PUT'){
-        res.end();
-        return;
-    }
-    else if(req.methdo === 'DELETE'){
-        res.end('Data is deleted');
-    }
+
     }
     catch(err){
         res.statusCode = 500;
