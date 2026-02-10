68a18514e567bd75685080740b666836ae0e9027
Rishank Kumar <rishankdhiman072006@gmail.com>
2026-02-10 13:54:15 +0530

    add logic to add expense for a given email and password

--- controllers/addExpense.js
+++ controllers/addExpense.js
@@ -1,2 +1,18 @@
-import db from './database.js';
+import db from '../db/database.js';
+import 'dotenv/config';
 
+const addExp = async (email , password) => {
+    try{
+        const [user] = await db.query(`SELECT * FROM ${process.env.USER_TABLE_NAME} WHERE usr_email = ? AND usr_pswd = ?`,[email,password]);
+        if(user.length === 0){
+            return 1;
+        }
+        else {
+            const userId = user[0].usr_id;
+            const [expenses] = await db.query(`SELECT * ${process.env.EXPENSE_TABLE_NAME} WHERE usr_id = ?` , userId);
+        }
+    }catch (error) {
+        console.log(`cannot able to fetch expense data ${error}`);
+        return 1;
+    }
+}
