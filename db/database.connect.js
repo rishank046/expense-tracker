ab9be06e3373bac43d0236992feef5edb9760237
Rishank Kumar <rishank046@gamil.com>
2026-04-07 21:59:04 +0530

    feat: add connection settings for database server

--- db/database.connect.js
+++ db/database.connect.js
@@ -1,14 +1,22 @@
-import mysql from 'mysql2'
-import 'dotenv/config';
+import mysql from "mysql2";
+import "dotenv/config";
+import fs from "node:fs";
 
-    const databaseServer = mysql.createPool({
-        user : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        database : process.env.DB_NAME,
-        
-        waitForConnections : true,
-        connectionLimit : 10,
-        queueLimit : 0
-    })
+const databaseServer = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-export default databaseServer.promise()
+  enableKeepAlive: true,
+  waitForConnections: true,
+  connectTimeout: 10000,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    ca: fs.readFileSync("./ca.pem"),
+  },
+});
+
+export default databaseServer.promise();
